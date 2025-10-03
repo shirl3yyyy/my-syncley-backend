ebf1a5568e4c2fe4efec4bed847a011aa16ddcef
Shirley Oduor <shirleycaren254@gmail.com>
2025-10-03 01:27:36 -0700

    modules and middleware  folder

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+//const authRoutes = require('./routes/auth');
 
 dotenv.config();
 
 const app = express();
 
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',  
+  credentials: true
+}));
+
+
+//app.use('/api/auth', authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
